9f58db28ba6e65e50cd03a70834dba62179cd580
Yeoeun Jeong <yeounjeong04@naver.com>
2024-09-04 00:45:13 +0900

    Title: 문자 제거하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240904/문자 제거하기/del-char.py
@@ -0,0 +1,14 @@
+a=input()
+c=list(a)
+while True:
+    b=int(input())
+    if b<len(c):
+        c.pop(b)
+        print("".join(c))
+        if len(c)==1:
+            break
+    elif b>len(c):
+        c.pop(-1)
+        print("".join(c))
+        if len(c)==1:
+            break
