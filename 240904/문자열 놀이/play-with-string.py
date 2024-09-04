be2f832ba772c0368a1022b748a7a9a6625ea64c
Yeoeun Jeong <yeounjeong04@naver.com>
2024-09-04 18:04:14 +0900

    Title: 문자열 놀이, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240904/문자열 놀이/play-with-string.py
+++ 240904/문자열 놀이/play-with-string.py
@@ -1,16 +1,17 @@
 s,q=input().split()
-p=list(s)
+k=list(s)
+
 q=int(q)
-for _ in range(q):
-    c,a,b=input().split()
-    arr=[c,a,b]
-    if arr[0]=='1':
-        p[int(a)-1]=s[int(b)-1]
-        p[int(b)-1]=s[int(a)-1]
-        print("".join(p))
-    elif arr[0]=='2':
-        for i in range(len(p)):
-            if p[i]==a:
-                p[i]=b
-                
-        print("".join(p))
+for i in range(q):
+    quest=input().split()
+    if quest[0]=='1':
+        a=int(quest[1])
+        b=int(quest[2])
+        k[a-1]=s[b-1]
+        k[b-1]=s[a-1]
+        print("".join(k))
+    else:
+        for i in range(len(k)):
+            if k[i]==quest[1]:
+                k[i]=quest[2]
+        print("".join(k))
