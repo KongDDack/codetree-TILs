18bf39bdf94e49f3222d7d9120c2f0dd8efa5e08
Yeoeun Jeong <yeounjeong04@naver.com>
2024-09-04 18:11:48 +0900

    Title: 문자열 계속 지우기, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240904/문자열 계속 지우기/keep-removing-string.py
@@ -0,0 +1,21 @@
+str_a=input()
+str_b=input()
+len_a=len(str_a)
+len_b=len(str_b)
+while True:
+    idx=-1
+    c=len_a-len_b+1
+    for i in range(c):
+        is_same=True
+        for j in range(len_b):
+            if str_a[i+j]!=str_b[j]:
+                is_same=False
+                break
+        if is_same:
+            idx=i
+            break
+    if idx==-1:
+        break
+    str_a=str_a[:idx]+str_a[idx+len_b:]
+    len_a=len(str_a)
+print(str_a)
