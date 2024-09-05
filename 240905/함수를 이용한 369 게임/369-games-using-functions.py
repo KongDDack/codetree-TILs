bb6040c9febaa5bedccbdc7286707f4167e56669
Yeoeun Jeong <yeounjeong04@naver.com>
2024-09-05 21:17:11 +0900

    Title: 함수를 이용한 369 게임, Time: 211ms, Memory: 26MB, Status: Passed - CodeTree

--- 240905/함수를 이용한 369 게임/369-games-using-functions.py
+++ 240905/함수를 이용한 369 게임/369-games-using-functions.py
@@ -3,7 +3,7 @@ a,b=map(int,input().split())
 def threesixnine(n):
     n=str(n)
     for i in range(len(n)):
-        if n[i]==3 or n[i]==6 or n[i]==9:
+        if n[i]=='3' or n[i]=='6' or n[i]=='9':
             return True
         
 
@@ -11,8 +11,11 @@ def threesixnine(n):
 def counting(n):
     return n%3==0 or threesixnine(n)
 
-    
+
 cnt=0
-for i in range(a,b+a):
+for i in range(a,b+1):
     if counting(i):
         cnt+=1
+   
+
+print(cnt)
