4ee7f4757ea79b2694936eb6f9847ca3b0cc2f82
Yeoeun Jeong <yeounjeong04@naver.com>
2024-09-05 21:31:49 +0900

    Title: 함수를 이용한 소수 판별, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240905/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240905/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -3,11 +3,13 @@ def prime_number(n):
     for i in range(2,n):
         if n%i==0:
             return False
-            
+
     return True
 
 cnt=0
 for i in range(a,b+1):
     if prime_number(i):
         cnt+=i
+    if a==1:
+        cnt-=1
 print(cnt)
