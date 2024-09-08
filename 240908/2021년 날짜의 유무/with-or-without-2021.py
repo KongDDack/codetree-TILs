998cad966534293fe95f84796d2f78646cbecdde
Yeoeun Jeong <yeounjeong04@naver.com>
2024-09-08 16:31:55 +0900

    Title: 2021년 날짜의 유무, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240908/2021년 날짜의 유무/with-or-without-2021.py
@@ -0,0 +1,18 @@
+M,D=map(int,input().split())
+
+def day_exist(a,b):
+    if a in [1,3,5,7,8,10,12]:
+        if b>31:
+            return False
+    elif a in [4,6,9,11]:
+        if b>30:
+            return False
+    elif a ==2:
+        if b>28:
+            return False
+    return True
+
+if day_exist(M,D):
+    print("Yes")
+else:
+    print("No")
