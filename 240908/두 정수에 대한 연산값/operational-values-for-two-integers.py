862dc71623993db9003c80243036f4f4521ed38b
Yeoeun Jeong <yeounjeong04@naver.com>
2024-09-08 20:51:29 +0900

    Title: 두 정수에 대한 연산값, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240908/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ 240908/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -3,10 +3,11 @@ def plus_multiple(a,b):
     if a>b:
         a=a+25
         b=b*2
-        return a,b
+        
     elif b>a:
         b=b+25
         a=a*2
-        return a,b
+    return a,b    
 
-print(*plus_multiple(a,b))
+a,b=plus_multiple(a,b)
+print(a,b)
